ae40f34e3e9ccc750292511ec7dd519cf52dece0
Yoverina <30620740+Yoverina@users.noreply.github.com>
2018-02-03 19:32:59 +0700

    try to get data from database

--- js/firebase.js
+++ js/firebase.js
@@ -8,11 +8,13 @@
 	messagingSenderId: "237361034617"
   };
   firebase.initializeApp(config);
-  console.log(firebase);
+//  console.log(firebase);
 //-----------------------------------------------------------
 
 //get database
   var database = firebase.database();
+
+//get html element
   var iname = document.getElementById("nama");
   var iuser = document.getElementById("uname");
   var iemail = document.getElementById("email");
@@ -20,6 +22,7 @@
   var e = document.getElementById("agama");
   var iasal;
   var gender;
+
  //  var radios = document.getElementsByName('gender');
  //  console.log(radios[0].value);
 	// for (var i = 0; i < radios.length; i++)
@@ -47,9 +50,6 @@
 
 	// desc = document.getElementById("description").text;
 
-
-console.log("djfjasdfkjkdsjfsd");
-
 	var data = {
 		name: iname,
 		username: iuser,
@@ -59,8 +59,29 @@ console.log("djfjasdfkjkdsjfsd");
 		gender: gender
   	}
 
-//reference database to specific tree -> user & push data to username
+//reference database to specific tree -> user & push data to user
 	var ref = database.ref('user');
 	ref.push(data);
  }
-  
+ 
+var ref1 = database.ref('user');
+ref1.on('value', gotData, errData);
+
+//coba ambil data dari database
+function gotData(data){
+	console.log(data.val());
+	var user = data.val();
+	var keys = Object.keys(user);
+	console.log(keys);
+	for (var i = 0; i < keys.length; i++) {
+		var k = keys[i];
+		var name = user[k].name;
+		var email = user[k].email;
+		console.log(name,email);
+	}
+}
+
+function errData(err){
+	console.log('Error!');
+	console.log(err);
+}
